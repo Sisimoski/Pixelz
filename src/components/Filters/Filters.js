f83a2d8e62ed56c968794967fd0157bf091b99b9
Pawel Damrath <pawel.damrath@student.po.edu.pl>
2021-03-17 16:26:15 +0100

    main - zmiany

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -6,30 +6,34 @@ import ikonapc from "../../media/pcb.png";
 import ikonans from "../../media/nsb.png";
 function Filters() {
     return (
-        <div className="filters">
+        <>
+            <div className="napis">
+                Choose Category
+            </div>
+            <div className="filters">
 
-            <div class="hexagon-wrapper">
-                <div class="hexagon">
-                    <i><img src={ikonax} /></i>
+                <div class="hexagon-wrapper">
+                    <div class="hexagon">
+                        <i><img src={ikonax} /></i>
+                    </div>
+                </div>
+                <div class="hexagon-wrapper">
+                    <div class="hexagon">
+                        <i><img src={ikonaps} /></i>
+                    </div>
+                </div>
+                <div class="hexagon-wrapper">
+                    <div class="hexagon">
+                        <i><img src={ikonapc} /></i>
+                    </div>
+                </div>
+                <div class="hexagon-wrapper">
+                    <div class="hexagon">
+                        <i><img src={ikonans} /></i>
+                    </div>
                 </div>
             </div>
-            <div class="hexagon-wrapper">
-                <div class="hexagon">
-                    <i><img src={ikonaps} /></i>
-                </div>
-            </div>
-            <div class="hexagon-wrapper">
-                <div class="hexagon">
-                    <i><img src={ikonapc} /></i>
-                </div>
-            </div>
-            <div class="hexagon-wrapper">
-                <div class="hexagon">
-                    <i><img src={ikonans} /></i>
-                </div>
-            </div>
-        </div>
-
+        </>
     )
 }
 
--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,9 +1,17 @@
 import Product from "./Product";
 import "./style.css"
+import SearchIcon from '@material-ui/icons/Search';
 const Products = ({ products }) => {
     return (
         <div>
+            <div className="searchContainer">
+                <form class>
+                    <input type="search" placeholder="Search" aria-label="Search" />
+                    <button type="submit"><SearchIcon></SearchIcon></button>
+                </form>
+            </div>
             <div className="product-container">
+
                 {products.map((product) => {
                     return <Product product={product} />
                 })}
