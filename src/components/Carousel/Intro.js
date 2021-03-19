65b87bdec082bc1defa804cee2f4a3aaf7df5353
Pawel Damrath <pawel.damrath@student.po.edu.pl>
2021-03-19 12:31:56 +0100

    zmiany

--- src/components/Carousel/Intro.js
+++ src/components/Carousel/Intro.js
@@ -1,56 +1,25 @@
 import React from 'react'
-import Carousel from "react-bootstrap/Carousel"
-import baner from "../../media/baner2.jpg"
-import pc from "../../media/pclogo.png"
-import nin from "../../media/nintendo.png"
-import xbox from "../../media/xboxlogo.jpg"
-import ps4 from "../../media/pslogo.png"
+import baner from "../../media/baner3.png"
+import { Container, Typography, Button, Grid } from "@material-ui/core";
 import "./style.css"
 function Intro() {
     return (
-        <div className="karuzela">
-            <Carousel fade>
-                <Carousel.Item interval={2000}>
-                    <img
-                        className="photo"
-                        src={baner}
-                        alt="First slide"
-                    />
-
-                </Carousel.Item>
-                <Carousel.Item interval={500}>
-                    <img
-                        className="photo"
-                        src={pc}
-                        alt="Second slide"
-                    />
-
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="photo"
-                        src={xbox}
-                        alt="Third slide"
-                    />
-
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="photo"
-                        src={nin}
-                        alt="Third slide"
-                    />
-
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="photo"
-                        src={ps4}
-                        alt="Third slide"
-                    />
-
-                </Carousel.Item>
-            </Carousel>
+        <div className="banner">
+            <Container>
+                <Grid container spacing={4}>
+                    <Grid className="brand" item sm={12}>
+                        <img src={baner} alt="Brand-tv" />
+                    </Grid>
+                    <Grid item xs={12} sm={12}>
+                        <p className="tekst">Skorzystaj z największego katalogu z grami, <br /> wybierz interesującą Cię pozycje,<br />  a następnie dodaj do koszyka.</p>
+                    </Grid>
+                    <Grid item xs={12} sm={12}>
+                        <Button className="shopping-button" href="#products">
+                            Zobacz katalog
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Container>
         </div>
     )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap';
 import logo from "../../media/baner.jpg"
+import { IconButton, Badge } from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
 import { NavLink } from 'react-router-dom';
-import login from "../../media/login.png"
+import { Link, useLocation } from "react-router-dom";
 import "./style.css"
-function SNavbar() {
+function SNavbar({ basketItems, totalCost }) {
+    const location = useLocation();
     return (
         <div className="navbarr">
             <Navbar className="NavbarFully" collapseOnSelect expand="lg" >
@@ -13,14 +16,41 @@ function SNavbar() {
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <div className="NavOptions">
                         <Nav className="mr-auto">
-                            <Nav.Link to="" activeClassName="nav-item nav-link active" className="Aboutus" >About Us</Nav.Link>
-                            <Nav.Link to="" activeClassName="nav-item nav-link active" className="Products" >Products</Nav.Link>
-                            <Nav.Link to="" activeClassName="nav-item nav-link active" className="Contact" >Contact</Nav.Link>
+                            <Nav.Link to="" className="Aboutus" >O nas</Nav.Link>
+                            <Nav.Link to="" className="Products" >Produkty</Nav.Link>
+                            <Nav.Link to="" className="Contact" >Kontakt</Nav.Link>
                             <div id="indicator"></div>
                         </Nav>
                     </div>
+                    <div className="basketContainer">
+                        {location.pathname === "/basket" ? (
+                            <div className="basket-wrapper">
+                                <h2>
+                                    Total cost: <strong>{totalCost}</strong>
+                                </h2>
+                            </div>
+                        ) : (
+                            <div className="basket-wrapper">
+                                <IconButton
+                                    component={Link}
+                                    to="/basket"
+                                    aria-label="Show basket contents"
+                                    color="inherit"
+                                >
+                                    <Badge badgeContent={basketItems} color="secondary">
+                                        <ShoppingCart className="custom-basket" />
+                                    </Badge>
+                                </IconButton>
+                            </div>
+                        )}
+                    </div>
 
-                    <Nav.Link to="" activeClassName="nav-item nav-link active" className="login" ><img className="loginicon" alt="img" src={login} /> <b>Login</b></Nav.Link>
+                    <form className="form-inline">
+                        <button className="btn1" type="button" id="myBtn">Zarejestruj się</button>
+                    </form>
+                    <form className="form-inline">
+                        <button className="btn" type="button" id="myBtn">Zaloguj się</button>
+                    </form>
 
                 </Navbar.Collapse>
             </Navbar>
