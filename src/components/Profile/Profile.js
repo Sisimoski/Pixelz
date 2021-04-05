bf19b7e19a59a8e33e00b32a76e48b043e436593
Pawel Damrath <pawel.damrath@student.po.edu.pl>
2021-04-05 12:26:23 +0200

    profil odnosnik

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+const Profile = () => {
+    return (
+        <div>
+            <h2>Profil uzytkownika</h2>
+        </div>
+    )
+}
+
+export default Profile
