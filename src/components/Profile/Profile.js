39b90c89b137230f16f6dc0c11448d9fc2cc4d6e
KamilBarczyk <kb7oker7@outlook.com>
2021-04-08 23:28:34 +0200

    Aktualizacja podstrony "Profil użytkownika"

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import './style.css';
 import Avatar from '../../media/avatar.jpg';
+
 const Profile = () => {
-    return (
-        <div>
-            <div class="container">
+return (
+
+<div>
+<div class="container">
     <div class="view-account">
         <section class="module">
             <div class="module-inner">
                 <div class="side-bar">
                     <div class="user-info">
-
-
-                    <img src="{Avatar}" alt=""/>
-
-
+                            
+                        <img src="{Avatar}" alt=""/>
 
                         <ul class="meta list list-unstyled">
                             <li class="name">JanKowalski1
                             </li>
                             <li class="email"><a href="#">jankowalski@gmail.com</a></li>
                         </ul>
-                    </div>
+                    </div> 
+                    
             		      <nav class="side-menu">
         				      <ul class="nav">
         					     <li class="active"><a href="#"><span class="fa fa-user"></span> Profil</a></li>
@@ -29,6 +29,7 @@ const Profile = () => {
         				      </ul>
         			     </nav>
                 </div>
+
                 <div class="content-panel">
                     <h2 class="title">Profil użytkownika</h2>
                     <form class="form-horizontal">
@@ -43,6 +44,7 @@ const Profile = () => {
                                     <button type="submit" class="btn btn-sm btn-default-alt pull-left">Zaktualizuj avatar</button>
                                 </div>
                             </div>
+
                             <div class="form-group">
                                 <label class="col-md-2 col-sm-3 col-xs-12 control-label">Nick</label>
                                 <div class="col-md-10 col-sm-9 col-xs-12">
@@ -56,6 +58,7 @@ const Profile = () => {
                                     <input type="text" class="form-control" value="Jan"/>
                                 </div>
                             </div>
+                            
                             <div class="form-group">
                                 <label class="col-md-2 col-sm-3 col-xs-12 control-label">Nazwisko</label>
                                 <div class="col-md-10 col-sm-9 col-xs-12">
@@ -63,6 +66,7 @@ const Profile = () => {
                                 </div>
                             </div>
                         </fieldset>
+
                         <fieldset class="fieldset">
                             <h3 class="fieldset-title">Informacje kontaktowe</h3>
                             <div class="form-group">
@@ -72,6 +76,7 @@ const Profile = () => {
                                 </div>
                             </div>
                         </fieldset>
+
                         <div class="form-group">
                             <div class="col-md-10 col-sm-9 col-xs-12 col-md-push-2 col-sm-push-3 col-xs-push-0">
                                 <input class="btn btn-primary" type="submit" value="Zmień hasło"/>
@@ -81,6 +86,7 @@ const Profile = () => {
                             </div>
                         </div>
                     </form>
+
                     <br/><br/>
                     <div id="historia">
                         <h2 class="title">Historia zakupow</h2>
@@ -90,7 +96,7 @@ const Profile = () => {
         </section>
     </div>
 </div>
-        </div>
+</div>
     )
 }
 
