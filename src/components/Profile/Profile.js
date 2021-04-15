0c6409a36adf93ed7ec70e8a5ddf2e0ae1b6e16c
Bartek Bielecki <47120754+bartoszbielecki@users.noreply.github.com>
2021-04-15 12:28:52 +0200

    Poprawki podstrony "Profil użytkownika"

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -11,48 +11,56 @@ return (
   <div class="container">
     <div class="view-account">
         <section class="module">
-        <h2 class="title">Profil użytkownika</h2>
+        <h2 class="module-title">Twoj profil</h2>
           <div class="avatar-box">
             <img class="avatar" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt=""/>
           </div>
         <div id="leftbox">
-          <br/>
-          <fieldset class="fieldset">
-            <h5 class="title">Adres e-mail</h5>
-            <h5 class="title">Nick</h5>
-          </fieldset>
-          <br/><br/>
-          <fieldset class="fieldset">
-            <h5 class="title">Imie</h5>
-            <h5 class="title">Nazwisko</h5>
-            <h5 class="title">Ulica i nr</h5>
-            <h5 class="title">Kod pocztowy</h5>
-            <h5 class="title">Miejscowość</h5>
-            <h5 class="title">Numer telefonu</h5>
-          </fieldset>
-        </div>
+        <h3>Dane konta</h3>
+          <div id="leftbox-module">
+            <div id="left-leftbox">
+              <br/>
+              <fieldset class="fieldset">
+                <h5 class="title">Adres e-mail</h5>
+                <h5 class="title">Nick</h5>
+              </fieldset>
+              <br/><br/>
+              <fieldset class="fieldset">
+                <h5 class="title">Imie</h5>
+                <h5 class="title">Nazwisko</h5>
+                <h5 class="title">Ulica i nr</h5>
+                <h5 class="title">Kod pocztowy</h5>
+                <h5 class="title">Miejscowość</h5>
+                <h5 class="title">Numer telefonu</h5>
+              </fieldset>
+            </div>
 
-        <div id="middlebox">
-          <fieldset class="fieldset">
-            <br/>
-            <h5>jankowalski1@poczta.pl</h5>
-            <h5>JanKowalski1</h5>
-          </fieldset>
-          <br/><br/>
-          <fieldset class="fieldset">
-            <h5>Jan</h5>
-            <h5>Kowalski</h5>
-            <h5>Malopolska 22</h5>
-            <h5>45-301</h5>
-            <h5>Opole</h5>
-            <h5>789 456 123</h5>
-          </fieldset>
-          <br/><br/>
+            <div id="middlebox">
+            </div>
 
-          <div class="form-group">
-            <input class="btn btn-primary" type="submit" value="Zaktualizuj profil"/>
+            <div id="right-leftbox">
+              <fieldset class="fieldset">
+                <br/>
+                <h5>jankowalski1@poczta.pl</h5>
+                <h5>JanKowalski1</h5>
+              </fieldset>
+              <br/><br/>
+              <fieldset class="fieldset">
+                <h5>Jan</h5>
+                <h5>Kowalski</h5>
+                <h5>Malopolska 22</h5>
+                <h5>45-301</h5>
+                <h5>Opole</h5>
+                <h5>789 456 123</h5>
+              </fieldset>
+              <br/><br/>
+
+              <div class="form-group">
+                <input class="btn btn-primary" type="submit" value="Zaktualizuj profil"/>
+              </div>
+              <br/><br/>
+            </div>
           </div>
-          <br/><br/>
         </div>
 
         <div id="rightbox">
@@ -63,6 +71,7 @@ return (
 
         </div>
       </section>
+      <br/><br/>
     </div>
   </div>
 </div>
