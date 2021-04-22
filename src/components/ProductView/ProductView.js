231157f3788859b7b3ccb5da46db7f881d926c0d
Pawel Damrath <pawel.damrath@student.po.edu.pl>
2021-04-22 19:57:48 +0200

    listAllProducts, better categories

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -10,7 +10,7 @@ const createMarkup = (text) => {
     return { __html: text };
 };
 
-const ProductView = ({ addProduct, categories }) => {
+const ProductView = ({ addProduct, categories, products }) => {
     const [product, setProduct] = useState({});
 
     const [quantity, setQuantity] = useState(1);
@@ -35,13 +35,17 @@ const ProductView = ({ addProduct, categories }) => {
             setQuantity(quantity + 1);
         }
     }
+    console.log("id produktu: ", products)
+    // console.log("id produktow all: ", products[0].categories[0].name)
+    //console.log("category", categories)
+    // console.log("id produktu kat:", categories[0].productsData[1].id)
     const [showSpinner, setShowSpinner] = useState(true);
     const loading = () => {
         setTimeout(() => {
             setShowSpinner(false);
-        }, 2000);
+        }, 5000);
         if (showSpinner) {
-            return <Spinner/>;
+            return <Spinner />;
         }
     };
     function category() {
@@ -63,6 +67,18 @@ const ProductView = ({ addProduct, categories }) => {
         }
         return categoryName.toUpperCase();
     }
+    function category2() {
+        if (products.length == 0)
+            categoryName = "Kategoria";
+        else {
+            var categoryName;
+            for (var i = 0; i < 25; i++) {
+                if (product.id == products[i].id)
+                    categoryName = products[i].categories[0].name
+            }
+        }
+        return categoryName;
+    }
     if (categories.length == 0) return loading();
     return (
         <div className="productview">
@@ -70,8 +86,8 @@ const ProductView = ({ addProduct, categories }) => {
                 <div className="row mx-5">
                     <div className="col">
                         <h2>{product.name}</h2>
-                        <h4 >{category()}</h4>
-                        <Image src={product.src} className="productScreens" fluid /> 
+                        <h4 >{category2()}</h4>
+                        <Image src={product.src} className="productScreens" fluid />
                         <div className="d-flex justify-content-between align-items-center mt-5">
                             <p className="productPrice font-weight-light">{product.price}</p>
                             <div className="ml-auto mr-3 d-flex align-items-center">
@@ -84,12 +100,12 @@ const ProductView = ({ addProduct, categories }) => {
                         </Button>
                         </div>
                         <hr />
-                   </div>
+                    </div>
                     <div className="productDescription">
                         <p><Typography dangerouslySetInnerHTML={createMarkup(product.description)} /></p>
                     </div>
                 </div>
-               
+
             </div>
         </div >
     )
