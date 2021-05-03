824f9477d8a560748248589164c72d6289d52408
Pawel Damrath <pawel.damrath@student.po.edu.pl>
2021-05-03 16:09:04 +0200

    Logowanie i rejestracja z zapisem danych

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,128 +1,136 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import './style.css';
 import reg from "../../media/reg.png";
+import { auth, generateUserDocument } from "../../lib/firebase";
 
-class register extends Component {
-    constructor() {
-        super();
+const Register = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [displayName, setDisplayName] = useState("");
+    const [names, setNames] = useState("");
+    const [surname, setSurname] = useState("");
+    const [street, setStreetName] = useState("");
+    const [postCode, setPostCode] = useState("");
+    const [cityName, setCityName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [error, setError] = useState(null);
 
-        this.state = {
-            Email: '',
-            Password: '',
-            Nick: '',
-            Name: '',
-            Surname: '',
-            Street: '',
-            PostalCode: '',
-            Town:'',
-            Phone:''
+    const createUserWithEmailAndPasswordHandler = async (event, email, password) => {
+        event.preventDefault();
+        try {
+            const { user } = await auth.createUserWithEmailAndPassword(email, password);
+            generateUserDocument(user, { displayName, names, surname, street, postCode, cityName, phoneNumber });
+        }
+        catch (error) {
+            setError('Error Signing up with email and password');
         }
 
-        this.Email = this.Email.bind(this);
-        this.Password = this.Password.bind(this);
-        this.Nick = this.Nick.bind(this);
-        this.Name = this.Name.bind(this);
-        this.Surname = this.Surname.bind(this);
-        this.Street = this.Street.bind(this);
-        this.PostalCode = this.PostalCode.bind(this);
-        this.Town = this.Town.bind(this);
-        this.Phone = this.Phone.bind(this);
-
-    }
-    Email(event) {
-        this.setState({ Email: event.target.value })
-    }
-    Password(event) {
-        this.setState({ Password: event.target.value })
-    }
-    Nick(event) {
-        this.setState({ Nick: event.target.value })
-    }
-    Name(event) {
-        this.setState({ Name: event.target.value })
-    }
-    Surname(event) {
-        this.setState({ Surname: event.target.value })
-    }
-    Street(event) {
-        this.setState({ Street: event.target.value })
-    }
-    PostalCode(event) {
-        this.setState({ PostalCode: event.target.value })
-    }
-   Town(event) {
-        this.setState({ Town: event.target.value })
-    }
-    Phone(event) {
-        this.setState({ Phone: event.target.value })
-    }
-    
-
-    render() {
-
+        setEmail("");
+        setPassword("");
+        setDisplayName("");
+        setNames("");
+        setSurname("");
+        setStreetName("");
+        setPostCode("");
+        setCityName("");
+        setPhoneNumber("");
+    };
+    const onChangeHandler = event => {
+        const { name, value } = event.currentTarget;
+        if (name === "userEmail") {
+            setEmail(value);
+        } else if (name === "userPassword") {
+            setPassword(value);
+        } else if (name === "displayName") {
+            setDisplayName(value);
+        } else if (name === "names") {
+            setNames(value);
+        } else if (name === "surname") {
+            setSurname(value);
+        } else if (name === "street") {
+            setStreetName(value);
+        } else if (name === "postCode") {
+            setPostCode(value);
+        } else if (name === "cityName") {
+            setCityName(value);
+        } else if (name === "phoneNumber") {
+            setPhoneNumber(value);
+        }
+    };
 
     return (
-   
+
         <div class="box">
-        <div class="kol1"></div>
-        <div class="kol2">
+            <div class="kol1"></div>
+            <div class="kol2">
 
-            <form action="#" method="POST"  name="signdata">
-            <n />
-            <img src={reg} width="150px" height="150px"/>
-            <h2> Dane konta</h2>
-        
-                <li>
-                    <label for="email"></label>
-                    <input class="inputFields" onChange={this.Email} type="e-mail" placeholder="Adres e-mail" pattern="[A-Za-z0-9-.,]{2,}@[A-Za-z0-9-]{2,}[.]{1}[a-zA-Z]{2,}" title="Podaj swój adres email" required />
-                </li>
-                <li>
-                    <label for="password"></label>
-                    <input class="inputFields" onChange={this.Password} type="password" id="password" placeholder="Hasło" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
-                        title="Hasło musi zawierać małą jak i duzą litere,znak specjalny oraz zawierać minimum 8 znaków" required />
-                </li>
-                <li>
-                    <label for="nick"></label>
-                    <input class="inputFields" onChange={this.Nick} type="text" id="nick" placeholder="Nick" pattern="(?=^.{5,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required />
-                </li>
-      
-        <br></br>
-        <h2> Dane kontaktowe</h2>
+                <form action="#" method="POST" name="signdata">
+                    <n />
+                    <img src={reg} width="150px" height="150px" />
+                    <h2> Dane konta</h2>
 
-                            <li>
-                                <label for="name"></label>
-                                <input class="inputFields" onChange={this.Name} type="text" placeholder="Imie" pattern="(?=^.{1,32}[A-Za-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]"  title="Podaj swoje imie" required />
-                            </li>
-                            <li>
-                                <label for="surname"></label>
-                                <input class="inputFields" onChange={this.Surname} type="text" id="surname" placeholder="Nazwisko" pattern="(?=^.{1,32}[A-Za-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]"  required />
-                            </li>
-                            <li>
-                                <label for="street"></label>
-                                <input class="inputFields" onChange={this.Street} type="text" id="street" placeholder="Ulica i numer" pattern="(?=^.{1,32}[A-Za-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]" required />
-                            </li>
-                            <li>
-                                <label for="postalCode"></label>
-                                <input class="inputFields" onChange={this.PostalCode} type="text" id="PostalCode" placeholder="Kod pocztowy" pattern="[0-9]{2}\-[0-9]{3}" required />
-                            </li>
-                            <li>
-                                <label for="Town"></label>
-                                <input class="inputFields" onChange={this.Town} type="text" id="Town" placeholder="Miejscowość" pattern="(?=^.{1,32}[A-Za-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]" required />
-                            </li>
-                            <li>
-                                <label for="Phone"></label>
-                                <input class="inputFields" onChange={this.Phone} type="tel" id="Phone" placeholder="Numer telefonu" pattern="[0-9]{9}" required />
-                            </li>
-                            <li id="center-btn">
-                     <input class="join-btn" type="submit" id="join-btn" name="join" alt="Join" value="Join"/>
-                </li>
-        </form>
- </div>
-        
-                    <div class="kol3"></div>
+                    <li>
+                        <label for="email"></label>
+                        <input class="inputFields" type="email" placeholder="Adres e-mail" name="userEmail" value={email} id="userEmail" onChange={event => onChangeHandler(event)}
+                            pattern="[A-Za-z0-9-.,]{2,}@[A-Za-z0-9-]{2,}[.]{1}[a-zA-Z]{2,}" title="Podaj swój adres email" required />
+                    </li>
+                    <li>
+                        <label for="password"></label>
+                        <input class="inputFields" type="password" id="userPassword" placeholder="Hasło" name="userPassword" value={password} onChange={event => onChangeHandler(event)}
+                            pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
+                            title="Hasło musi zawierać małą jak i duzą litere,znak specjalny oraz zawierać minimum 8 znaków" required />
+                    </li>
+                    <li>
+                        <label for="nick"></label>
+                        <input class="inputFields" type="text" id="displayName" placeholder="Nick" name="displayName" value={displayName} onChange={event => onChangeHandler(event)}
+                            pattern="(?=^.{1,32}[A-Za-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]" required />
+                    </li>
+
+                    <br></br>
+                    <h2> Dane kontaktowe</h2>
+
+                    <li>
+                        <label for="name"></label>
+                        <input class="inputFields" type="text" placeholder="Imie" id="names" name="names" value={names} onChange={event => onChangeHandler(event)}
+                            pattern="(?=^.{1,32}[A-Za-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]" title="Podaj swoje imie" required />
+                    </li>
+                    <li>
+                        <label for="surname"></label>
+                        <input class="inputFields" type="text" id="surname" placeholder="Nazwisko" name="surname" value={surname} onChange={event => onChangeHandler(event)}
+                            pattern="(?=^.{1,32}[A-Za-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]" required />
+                    </li>
+                    <li>
+                        <label for="street"></label>
+                        <input class="inputFields" type="text" id="street" placeholder="Ulica i numer" name="street" value={street} onChange={event => onChangeHandler(event)}
+                            pattern="(?=^.{1,32}[A-Za-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]" required />
+                    </li>
+                    <li>
+                        <label for="postalCode"></label>
+                        <input class="inputFields" type="text" id="postCode" placeholder="Kod pocztowy" name="postCode" value={postCode} onChange={event => onChangeHandler(event)}
+                            pattern="[0-9]{2}\-[0-9]{3}" required />
+                    </li>
+                    <li>
+                        <label for="Town"></label>
+                        <input class="inputFields" type="text" id="cityName" placeholder="Miejscowość" name="cityName" value={cityName} onChange={event => onChangeHandler(event)}
+                            pattern="(?=^.{1,32}[A-Za-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]" required />
+                    </li>
+                    <li>
+                        <label for="Phone"></label>
+                        <input class="inputFields" type="tel" id="phoneNumber" placeholder="Numer telefonu" name="phoneNumber" value={phoneNumber} onChange={event => onChangeHandler(event)}
+                            pattern="[0-9]{9}" required />
+                    </li>
+                    <li id="center-btn">
+                        <input class="join-btn" type="submit" id="join-btn" name="join" alt="Join" value="Join" onClick={event => {
+                            createUserWithEmailAndPasswordHandler(event, email, password);
+                        }} />
+                    </li>
+                </form>
+            </div>
+
+            <div class="kol3"></div>
         </div>
-        )
-    }
+    )
 }
 
-export default register;
+export default Register;
